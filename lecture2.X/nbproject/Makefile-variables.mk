3a8c2f22670293ea799b793da859accae546e2c8
Garrett Vanhoy <gvanhoy@email.arizona.edu>
2015-08-27 10:13:07 -0700

    Second Lecture.

---
+++ lecture2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=lecture2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lecture2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lecture2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lecture2.x.tar
