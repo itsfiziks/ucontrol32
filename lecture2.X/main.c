ff324772457dc800f9e4231936aa7fb70c278f93
Garrett Vanhoy <gvanhoy@email.arizona.edu>
2015-08-27 10:45:28 -0700

    State machine implementation.

--- lecture2.X/main.c
+++ lecture2.X/main.c
@@ -11,22 +11,30 @@
 #define OUTPUT 0
 #define INPUT 1
 
+typedef enum stateTypeEnum{
+    led_on, led_off
+} stateType;
 /*
  * 
  */
 int main() {
 
+    stateType state = led_on;
     TRISDbits.TRISD0 = OUTPUT;
     TRISDbits.TRISD6 = INPUT;
     CNPUDbits.CNPUD6 = 1;
     LATDbits.LATD0 = 1;
     
     while(1){
-        if(PORTDbits.RD6 == 0){
-            LATDbits.LATD0 = 1;
-        }
-        else{
-            LATDbits.LATD0 = 0;
+        switch(state){
+            case led_on:
+                LATDbits.LATD0 = 1;
+                if(PORTDbits.RD6 == 0) state = led_off;
+                break;
+            case led_off:
+                LATDbits.LATD0 = 0;
+                if(PORTDbits.RD6 == 1) state = led_on;
+                break;
         }
     }
     
