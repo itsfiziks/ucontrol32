d88b8b63a845aa231368064e94b240c93ff46978
itsfiziks <gvanhoy@arizona.edu>
2015-09-01 13:16:34 -0700

    Enable overall CN first.

--- lecture3-pinIssues.X/main.c
+++ lecture3-pinIssues.X/main.c
@@ -9,53 +9,30 @@
 #include <sys/attribs.h>
 #include "config.h"
 
-
-/*
- * 
- */
 int main() {
     
-    int i = 0;
-    
-    //Set TRD1 pin as an output
-    TRISGbits.TRISG14 = 0;
+    TRISDbits.TRISD2 = 0;       //Initialize LED 2
+    LATDbits.LATD2 = 0;         // Default OFF
 
-    //set TRD2 as an input
-    TRISGbits.TRISG12 = 1;
-    
-    TRISDbits.TRISD2 = 0;
-    
-    //Enable the internal pull=up resistor
-    CNPUGbits.CNPUG12 = 1;
-    
-    //Enable Change notifications
-    CNENGbits.CNIEG12 = 1;
-    
-    //Enable the change notification ISR
-    IEC1bits.CNGIE = 1;
-    
-    //Put the flag down
-    IFS1bits.CNGIF = 0;
-    
-    //Set interrupt priority
-    
-    //"send" a zero
-    LATGbits.LATG14 = 0;
-    
-    LATDbits.LATD2 = 0;
+    TRISGbits.TRISG14 = 0;      //Set TRD1 pin as an output
+    TRISGbits.TRISG12 = 1;      //set TRD2 as an input
 
+    CNCONGbits.ON = 1;          //Turn ON change notifications
+    CNPUGbits.CNPUG12 = 1;      //Enable the internal pull-up resistor
+    CNENGbits.CNIEG12 = 1;      //Enable Change notifications
+
+    LATGbits.LATG14 = 0;        //start a zero
+    
     while(1){
-        //Artificially change the pin value
-        LATGbits.LATG14 = 0;
+        //Artificially change the pin value (like a pulse))
         LATGbits.LATG14 = 1;
+        LATGbits.LATG14 = 0;
+        
+        //Check if a CN has been triggered
         if(IFS1bits.CNGIF == 1){
             LATDbits.LATD2 = 1;
         }
     }
     
     return 0;
-}
-
-void __ISR(_TIMER_1_VECTOR, IPL7SRS) _T1Interrupt(){
-    
 }
